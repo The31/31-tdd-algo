04331f1127833e0a7cd85e0189cc65f06bd9689a
anotherFeng <regnagleppod@gmail.com>
2018-07-25 14:11:37 -0700

    add 4 more challenges

---
+++ easy/highestCountChar/index.js
@@ -0,0 +1,10 @@
+// --- Directions
+// Given a string, return the character that is most
+// commonly used in the string.
+// --- Examples
+// highestCountChar("abcccccccd") === "c"
+// highestCountChar("apple 1231111") === "1"
+
+function highestCountChar(str) {}
+
+module.exports = highestCountChar;
